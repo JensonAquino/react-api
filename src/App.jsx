e9f98f355208bffd9bf7d7bfce559cad7f41b4d4
JensonAquino <jensonaquino2622@gmail.com>
2025-01-09 00:46:18 +0100

    feat:esercizzio completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import AppCard from "./components/AppCard"
 const apiUrl = import.meta.env.VITE_API_URL;
 const initailData = {
   title: "",
@@ -59,37 +60,39 @@ const getPosts= () => {
   return (
     <>
       <div className="container">
-        {posts.length > 0 ? (
-          posts.map((post) => (
-            <div key={post.id}>
-              <h3 className="text-center text-black">Ultimi post</h3>
-              <div className="text-center mb-3">
-                {post.image && <img src={post.image} alt="" className="img-fluid" />}
-              </div>
-              <div className="card container">
-                <div className="card-body text-center">
-                  <h5>{post.title}</h5>
-                  <img src={`${apiUrl}/${post.image}`} alt="" />
-                  <p>{post.content}</p>
-                  <p><strong>Categoria:</strong> {post.category}</p>
-                  <p>
-                    <strong>Stato:</strong> {post.isPublished ? "Pubblicato" : "Non pubblicato"}
-                  </p>
+      {
+        posts.length > 0 ? (
+            posts.map((post) => (
+                <div key={post.id}>
+                    <h3 className="text-center text-black">Ultimi post</h3>
+                    <div className="text-center mb-3">
+                        {post.image && <img src={post.image} alt="" className="img-fluid" />}
+                    </div>
+                    <div className="card container">
+                        <div className="card-body text-center">
+                            <h5>{post.title}</h5>
+                            <img src={`${apiUrl}/${post.image}`} alt="" />
+                            <p>{post.content}</p>
+                            <p><strong>Categoria:</strong> {post.category}</p>
+                            <p>
+                                <strong>Stato:</strong> {post.isPublished ? "Pubblicato" : "Non pubblicato"}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="container text-center">
+                        <button
+                            className="btn btn-danger mt-2"
+                            onClick={() => (deletePost(post.id))}
+                        >
+                            Elimina Post
+                        </button>
+                    </div>
                 </div>
-              </div>
-              <div className="container text-center">
-                <button
-                  className="btn btn-danger mt-2"
-                  onClick={() => (deletePost(post.id))}
-                >
-                  Elimina Post
-                </button>
-              </div>
-            </div>
-          ))
+            ))
         ) : (
-          <p className="text-center text-black">Articoli non esistenti</p>
-        )}
+        <p className="text-center text-black">Articoli non esistenti</p>
+    )
+    }
         <form onSubmit={sendPost}>
           <div className="mb-3">
             <div className="text-center">
